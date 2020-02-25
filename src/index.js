7c468fbc837884ac12faed4975987902d9177557
Dmitriy <krudda@yandex.ru>
2020-02-26 00:09:57 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let pos = Math.abs(n);
+    let arr = pos.toString().split('');
+    let reverseNumber = +arr.reverse().join('');
+    return reverseNumber;
 }
